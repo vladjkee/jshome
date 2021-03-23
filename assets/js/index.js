bb94b8c03f35a73fbfd278030ce12c64d748bf52
vlad <vladiktarasenko@gmail.com>
2021-03-23 23:48:57 +0200

    homeworkd

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+// let user = prompt();
+// alert(user);
+// if(isNaN(result)){
+  //   return "ne verno huesos";
+  //  }
+  //  return result
+
+// const text1 = prompt("enter number 1:");
+// const text2 = prompt("enter number 2:");
+// const firstFunc = function(val1, val2){
+//   const result = Number(val1) + Number(val2);
+//   return isNaN(result) ? "ne verno hueso" : result;
+// }
+
+// const sum = firstFunc(text1,  text2);
+// console.log(sum)
+
+const text1 = prompt("enter number 1:");
+const text2 = prompt("enter number 2:");
+
+
+ const getMaxValue = function(val1, val2){
+   if(isNaN(val1 - val2)){
+    return false;
+   }
+   if (val1 > val2){
+     return val1;
+   }
+   return val2;
+ }
+console.log("Max value is: ", getMaxValue(text1, text2));
+
+const text3 = prompt("enter number 1:");
+const text4 = prompt("enter number 2:");
+
+
+ const getMinValue = function(val1, val2){
+   if(isNaN(val1 - val2)){
+    return false;
+   }
+   if (val1 < val2){
+     return val1;
+   }
+   return val2;
+ }
+console.log("Min value is: ",getMinValue(text3, text4));
+
+const text5 = prompt("enter number is Even:");
+
+ const isEven = function(number){
+ return number % 2 === 0;
+} 
+console.log("is Even number: ",isEven(text5));
